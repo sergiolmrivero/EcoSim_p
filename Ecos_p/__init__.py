80080c10e934e7b11e23e8766ef106e3a5ea0e6d
Sergio Rivero <sergiolmrivero@gmail.com>
2020-03-18 18:15:46 -0300

    Include Ecos_p and kernel modules

--- Ecos_p/__init__.py
+++ Ecos_p/__init__.py
@@ -6,13 +6,7 @@ Python Economic ABM
 import datetime
 
 
-from Ecos_p.kernel.simulation import Simulation
-from Ecos_p.kernel.model import Model
-from Ecos_p.kernel.basicSpaces import Space
-import Ecos_p.kernel.basicAgents
-
-
-__title__ = 'kernel'
+__title__ = 'Ecos_p'
 __version__ = '0.0.0.Alpha'
 __license__ = 'gnu'
 __copyright__ = 'Copyright %s Ecos_p Team' % datetime.date.today().year
