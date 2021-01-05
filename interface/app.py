a43ecfc0455331f77062fa84e3b730f88827b188
brunoconde07 <brunoconde.ufpa@gmail.com>
2021-01-05 03:10:49 -0300

    /parametrization route to receive the json by the user through the web interface. Svaing the json is ok, but is missing to save in the correct place (select model directory)

--- interface/app.py
+++ interface/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, render_template
 from flask_restful import Resource, Api
 import os
 
-from modules.interface import ParameterProvider, ExecuteSimulationFromJson
+from modules.interface import ParameterProvider, ExecuteSimulationFromJson, Json
 
 """
 HTTP Status                                                   
@@ -10,6 +10,9 @@ HTTP Status
 201 - Created
 404 - Error
 """
+
+# storage the model name to be simulated. The user entry with this by the interface
+MODEL: str = None
 
 app = Flask(__name__)
 app.config['PROPAGATE_EXCEPTIONS'] = True # To allow flask propagating exception even if debug is set to false on app
@@ -26,6 +29,22 @@ def homepage():
 
     return render_template('/homepage.html')
 
+@app.route('/parametrization', methods=['GET', 'POST'])
+def parametrization():
+    """
+    1.Recieve the model to be simulated by the homepage
+    2.Open an text area to the user
+        setting up the json file
+        which defines the
+        simulation parameters
+    3.Set the path to model
+    """
+
+    MODEL = request.form["model"]
+    Json.set_path_to_model(model_name=MODEL, path_to_app_server=os.getcwd())
+
+    return render_template('/parametrization.html')
+
 
 @app.route('/simulation', methods=['GET', 'POST'])
 def simulation():
@@ -34,9 +53,10 @@ def simulation():
     Execute the simulation (of the choosen model) itself
     Render the simulation web page
     """
-
-    model_name = request.form["model"]    
-    executeSimulationFromJson.exec_simulation(model_name)
+    
+    # executeSimulationFromJson.exec_simulation(model_name)
+    parameters: str =  request.form["textarea-parameters"]
+    Json.save_parameters(parameters=parameters, model_name=MODEL)
 
     return render_template('/simulation.html')
 
