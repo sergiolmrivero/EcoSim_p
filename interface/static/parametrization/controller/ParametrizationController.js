f746c14b73d3da6b1147769f3891447706c9f766
brunoconde07 <brunoconde.ufpa@gmail.com>
2021-02-16 19:33:04 -0300

    (static/parametrization/controller/ParametrizationController.js, at interface/) -> hide model, spaces, agents, and observers div at the initialization

--- interface/static/parametrization/controller/ParametrizationController.js
+++ interface/static/parametrization/controller/ParametrizationController.js
@@ -1,26 +1,41 @@
 class ParametrizationController {
 
 	constructor(){
-		this._simulationMenuEl = document.querySelector("#simulation");	
+		this._simulationMenuEl = document.querySelector("#simulation");
+		this._modelMenuEl = document.querySelector("#model");
+		this._spacesMenuEl = document.querySelector("#spaces");
+		this._agentsMenuEl = document.querySelector("#agents");
+		this._observersMenuEl = document.querySelector("#observers");
+
 		this.initializate();
 
 	}
 
-	addEventListenerToToMenuItemToHideCorrespondingDiv(menuItemEl, id_div) {
+	addEventListenerToMenuItemToHideCorrespondingDiv(menuItemEl, id_div) {
 		menuItemEl.addEventListener("click", e => {
 			this.hideElement(id_div);
 		});		
 	}
 
+	addEventListenerToMenuItemToShowCorrespondingDiv(menuItemEl, id_div) {
+		menuItemEl.addEventListener("click", e => {
+			this.showElement(id_div);
+		});		
+	}
+
 	initializate() {	
 
-		// simulation
-		this.addEventListenerToToMenuItemToHideCorrespondingDiv(this._simulationMenuEl, "div-simulation");
+		// hide model div
+		this.hideElement('div-model');
 
-		// model
-		// spaces
-		// agents
-		// observers
+		// hide spaces div
+		this.hideElement('div-spaces');
+
+		// hide agents div
+		this.hideElement('div-agents');
+
+		// hide observers div
+		this.hideElement('div-observers');
 
 	}
 
