6978888ad4430b4cf8edf91d109e318cd6978f22
brunoconde07 <brunoconde.ufpa@gmail.com>
2021-02-16 20:18:04 -0300

    (static/parametrization/controller/ParametrizationController.js, at interface/) -> hide model, spaces, agents, and observers div at the initialization and showing them when click in some menu item

--- interface/static/parametrization/controller/ParametrizationController.js
+++ interface/static/parametrization/controller/ParametrizationController.js
@@ -19,23 +19,35 @@ class ParametrizationController {
 
 	addEventListenerToMenuItemToShowCorrespondingDiv(menuItemEl, id_div) {
 		menuItemEl.addEventListener("click", e => {
-			this.showElement(id_div);
+			this.showElement(id_div);			
 		});		
 	}
 
 	initializate() {	
 
 		// hide model div
-		this.hideElement('div-model');
+		// this.hideElement('div-model');
 
 		// hide spaces div
-		this.hideElement('div-spaces');
+		// this.hideElement('div-spaces');
 
 		// hide agents div
-		this.hideElement('div-agents');
+		// this.hideElement('div-agents');
 
 		// hide observers div
-		this.hideElement('div-observers');
+		// this.hideElement('div-observers');
+
+		let arrMainDivEl = Array.prototype.slice.call(document.querySelectorAll(".main > div"));
+		arrMainDivEl.slice(1, arrMainDivEl.length).forEach(divEl => {
+			this.hideElement(divEl.id);
+		});
+
+		document.querySelectorAll(".sidenav > a").forEach(aEl => {
+			this.addEventListenerToMenuItemToShowCorrespondingDiv(aEl, "div-" + aEl.id);
+			// this.addEventListenerToMenuItemToHideCorrespondingDiv(aEl, "div-" + aEl.id);
+		});
+
+
 
 	}
 
