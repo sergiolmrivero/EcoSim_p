33e7e9d441774c5f8e3cc47c71cb48c7282114f5
brunoconde07 <brunoconde.ufpa@gmail.com>
2021-02-17 06:49:42 -0300

    (ParametrizationController_test.js, parametrization_test.js, and parametrization.html, at interface/templates/parametrization) -> [working] javascript load correctly without using flask. js files loaded from system files instead

---
+++ interface/templates/parametrization/ParametrizationController_test.js
@@ -0,0 +1,58 @@
+class ParametrizationController {
+
+	constructor(){
+		this._simulationMenuEl = document.querySelector("#simulation");
+		this._modelMenuEl = document.querySelector("#model");
+		this._spacesMenuEl = document.querySelector("#spaces");
+		this._agentsMenuEl = document.querySelector("#agents");
+		this._observersMenuEl = document.querySelector("#observers");
+
+		this._arrMainDivEl = Array.prototype.slice.call(document.querySelectorAll(".main > div"));
+
+		this.initializate();
+
+	}
+
+	addEventListenerToMenuItemToHideCorrespondingDiv(menuItemEl, id_div) {
+		menuItemEl.addEventListener("click", e => {
+			this.hideElement(id_div);
+		});		
+	}
+
+	addEventListenerToMenuItemToShowCorrespondingDiv(menuItemEl, id_div) {
+		menuItemEl.addEventListener("click", e => {
+			this.showElement(id_div);			
+		});		
+	}
+
+	initializate() {	
+		
+		this._arrMainDivEl.slice(1, this._arrMainDivEl.length).forEach(divEl => {
+			this.hideElement(divEl.id);
+		});
+
+		document.querySelectorAll(".sidenav > a").forEach(aEl => {
+			this.addEventListenerToMenuItemToShowCorrespondingDiv(aEl, "div-" + aEl.id);
+			// this.addEventListenerToMenuItemToHideCorrespondingDiv(aEl, "div-" + aEl.id);
+		});
+
+	}
+
+	showElement(i) {
+
+		this._arrMainDivEl.forEach(divEl => {
+
+			// hiding displayed elements before show the clicked div corresponding
+			// to clicked menu item
+			if (divEl.style.display == "block") divEl.style.display = "none";
+
+		});
+
+		document.querySelector("#" + i).style.display = "block";
+	}
+
+	hideElement(i) {
+		document.querySelector("#" + i).style.display = "none";
+	}
+	
+}
