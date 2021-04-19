c779793221933926c255af5ca1f73a060b1fd786
brunoconde07 <brunoconde.ufpa@gmail.com>
2021-04-19 19:09:01 -0300

    Adding and removing some files

---
+++ interface/static/parametrization/models/Simulation.js
@@ -0,0 +1,11 @@
+class Simulation {
+
+	constructor(simulation_name, simulation_model, simulation_parameters, model_name) {
+
+		this.simulation_name = simulation_name;
+		this.simulation_model = simulation_model;
+		this.simulation_parameters = simulation_parameters;
+		this.model_name = model_name;
+
+	}
+}
