a77fdf5a3f86254247c0565808fc34c97e3ecfa5
brunoconde07 <brunoconde.ufpa@gmail.com>
2021-04-22 13:48:51 -0300

    (common/database.py, at/interface) -> get examples and models

--- interface/common/database.py
+++ interface/common/database.py
@@ -9,14 +9,18 @@ class Database:
     """    
        
     @staticmethod
-    def get_models() -> Dict:
+    def get_models() -> Dict[str]:
         """
         Get examples from the examples directory
+        Get models from the examples directory
+        -> return: Dict[examples and models]
         """     
-        examples_files: List[str] = []        
-        path_examples = os.path.join(os.sep.join(os.getcwd().split(os.sep)[:-1]), 'examples')     
-        examples_files.extend(os.listdir(path_examples))
+        example_and_model_files: List[str] = []        
+        path_examples: str = os.path.join(os.sep.join(os.getcwd().split(os.sep)[:-1]), 'examples')
+        path_models: str = os.path.join(os.sep.join(os.getcwd().split(os.sep)[:-1]), 'models')
+        example_and_model_files.extend(os.listdir(path_examples))
+        example_and_model_files.extend(os.listdir(path_models))
         
         return {
-            "models": list(set(filter(lambda file: '__' not in file, examples_files)))
+            "models": list(set(filter(lambda file: '__' not in file, example_and_model_files)))
         }        
